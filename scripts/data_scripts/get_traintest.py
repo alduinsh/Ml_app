02546036dae90e8586c5ef10c0067a0625fc7133
aldiunsh <lucy.mihaylichenko@gmail.com>
2023-10-03 13:18:13 +0000

    Data pipeline

--- scripts/data_scripts/get_traintest.py
+++ scripts/data_scripts/get_traintest.py
@@ -4,15 +4,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 # Загрузка датасета из папки "raw"
-dataset_path = 'data/raw/ds_salaries.csv'
+dataset_path = 'data/raw_tt/ds_salaries.csv'
 df = pd.read_csv(dataset_path)
 
 # Разделение на train и test
 train, test = train_test_split(df, test_size=0.2, random_state=42)
 
 # Проверка и создание папки "raw_tt"
-os.makedirs('data/raw_tt', exist_ok=True)
+os.makedirs('data/raw', exist_ok=True)
 
 # Сохранение train и test в файлы
-train.to_csv('data/raw_tt/train.csv', index=False)
-test.to_csv('data/raw_tt/test.csv', index=False)
+train.to_csv('data/raw/train.csv', index=False)
+test.to_csv('data/raw/test.csv', index=False)
